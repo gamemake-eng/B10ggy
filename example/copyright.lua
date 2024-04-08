5c89bc7cc40c5b23995428a21bec27818174c1ca
gamemake-eng <mohinathsivaneswaran@gmail.com>
2024-04-08 16:39:47 -0400

    Update copyright.lua, settings.json, and main.lua

--- example/copyright.lua
+++ example/copyright.lua
@@ -1,7 +1,7 @@
 m = {}
-
+m.is_widget = true
 function m:run(k,v)
-	local fs = v
+	local fs = "Copyright"..v.author.." @year @symbolC"
 	fs = fs:gsub("@year", os.date("%Y",os.time()))
 	fs = fs:gsub("@symbolC", "©")
 	return fs
